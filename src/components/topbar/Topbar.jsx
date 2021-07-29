10ee787a87b9c779c922f42e32990460f8319de7
maidul-bappy <maidul.bappy.001@gmail.com>
2021-07-30 03:20:52 +0600

    Hello Blog UI Created Only

---
+++ src/components/topbar/Topbar.jsx
@@ -0,0 +1,61 @@
+import { faFacebookSquare, faInstagramSquare, faPinterestSquare, faTwitterSquare } from "@fortawesome/free-brands-svg-icons";
+import { faSearch } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { Link } from "react-router-dom";
+import "./topbar.css";
+
+export default function Topbar() {
+  const user = true;
+  return (
+    <div className="top">
+      <div className="topLeft">
+        <FontAwesomeIcon className="topIcon" icon={faFacebookSquare}/>
+        <FontAwesomeIcon className="topIcon" icon={faInstagramSquare}/>
+        <FontAwesomeIcon className="topIcon" icon={faPinterestSquare}/>
+        <FontAwesomeIcon className="topIcon" icon={faTwitterSquare}/>
+      </div>
+      <div className="topCenter">
+        <ul className="topList">
+          <li className="topListItem">
+            <Link className="link" to="/">
+              HOME
+            </Link>
+          </li>
+          <li className="topListItem">ABOUT</li>
+          <li className="topListItem">CONTACT</li>
+          <li className="topListItem">
+            <Link className="link" to="/write">
+              WRITE
+            </Link>
+          </li>
+          {user && <li className="topListItem">LOGOUT</li>}
+        </ul>
+      </div>
+      <div className="topRight">
+        {user ? (
+          <Link className="link" to="/settings">
+            <img
+              className="topImg"
+              src="https://images.pexels.com/photos/1858175/pexels-photo-1858175.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
+              alt=""
+            />
+          </Link>
+        ) : (
+          <ul className="topList">
+            <li className="topListItem">
+              <Link className="link" to="/login">
+                LOGIN
+              </Link>
+            </li>
+            <li className="topListItem">
+              <Link className="link" to="/register">
+                REGISTER
+              </Link>
+            </li>
+          </ul>
+        )}
+        <FontAwesomeIcon className="topSearchIcon" icon={faSearch}/>
+      </div>
+    </div>
+  );
+}
---
+++ src/pages/settings/Settings.jsx
@@ -0,0 +1,45 @@
+import "./settings.css";
+import Sidebar from "../../components/sidebar/Sidebar";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faUserCircle } from "@fortawesome/free-solid-svg-icons";
+
+export default function Settings() {
+  return (
+    <div className="settings">
+      <div className="settingsWrapper">
+        <div className="settingsTitle">
+          <span className="settingsTitleUpdate">Update Your Account</span>
+          <span className="settingsTitleDelete">Delete Account</span>
+        </div>
+        <form className="settingsForm">
+          <label>Profile Picture</label>
+          <div className="settingsPP">
+            <img
+              src="https://images.pexels.com/photos/6685428/pexels-photo-6685428.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
+              alt=""
+            />
+            <label htmlFor="fileInput">
+              <FontAwesomeIcon className="settingsPPIcon" icon={faUserCircle} />{" "}
+            </label>
+            <input
+              id="fileInput"
+              type="file"
+              style={{ display: "none" }}
+              className="settingsPPInput"
+            />
+          </div>
+          <label>Username</label>
+          <input type="text" placeholder="User" name="name" />
+          <label>Email</label>
+          <input type="email" placeholder="user@gmail.com" name="email" />
+          <label>Password</label>
+          <input type="password" placeholder="Password" name="password" />
+          <button className="settingsSubmitButton" type="submit">
+            Update
+          </button>
+        </form>
+      </div>
+      <Sidebar />
+    </div>
+  );
+}
